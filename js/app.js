8556cabad2c171664d07e4374478f817600441c8
luissalcedo <salcedokosma@gmail.com>
2018-06-02 13:06:56 +0100

    added player and allEnemies variable, now the player is displayed on the bottom of the screen

--- js/app.js
+++ js/app.js
@@ -49,7 +49,8 @@ Player.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies =[];
+var player = new Player(200, 400, 50);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
